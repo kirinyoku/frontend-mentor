11dc6ab3efb561c45d2037541a80300ca2b2d85e
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-01-31 21:50:44 +0200

    Update

--- wordle/app/js/script.js
+++ wordle/app/js/script.js
@@ -1,5 +1,12 @@
-console.log('HELLO');
+const letterPattern = /^[A-Za-z][A-Za-z0-9]*$/;
 
-const test = () => {
-	console.log('this is a test');
-};
+let currentGuessCount = 1;
+let currentLetters = document.querySelector('#guess' + currentGuessCount);
+
+document.addEventListener('keydown', (e) => {
+	if (e.key.length === 1 && letterPattern.test(e.key)) {
+		console.log('true');
+	} else {
+		console.log('false')
+	}
+});
