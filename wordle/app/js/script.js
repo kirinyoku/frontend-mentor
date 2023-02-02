82097e0e59946fc3545ba2de21f5c3f9cedc4b54
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-02-02 19:40:21 +0200

    Update

--- wordle/app/js/script.js
+++ wordle/app/js/script.js
@@ -1,69 +1,116 @@
 // guess counter
 let currentGuessCount = 1;
 // tile counter
-let currentTileNumber = 0;
+let tileNumber = 0;
 // current row of guess
 let currentGuess = document.querySelector(`#guess${currentGuessCount}`);
 // array of tiles in the current row of guess
 let guessTiles = document.querySelectorAll(`#guess${currentGuessCount} .guess__tile`);
-
+// controller for event listener 
+const controller = new AbortController();
+const { signal } = controller;
 // regular expression to test a string for a letter
 const letterPattern = /^[A-Za-z][A-Za-z0-9]*$/;
 
+const words = ['cream'];
+
 // adding animation-delay to the tile wrappers (to animate the flipping of tiles)
-for (let i = 1; i <= 6; i++) {
-	let delay = 0;
-	const tileWrapperList = document.querySelectorAll(`#guess${i} > div`);
-	for (let j = 0; j < tileWrapperList.length; j++){
-		tileWrapperList[j].style.animationDelay = `${delay}ms`;
-		delay += 100;
-	}
+const addAnimDelay = () => {
+	for (let i = 1; i <= 6; i++) {
+		let delay = 0;
+		const tileWrapperList = document.querySelectorAll(`#guess${i} > div`);
+		for (let j = 0; j < tileWrapperList.length; j++){
+			tileWrapperList[j].style.animationDelay = `${delay}ms`;
+			delay += 150;
+		};
+	};
+};
+
+const addAnimation = (element, animName) => {
+	element.dataset.animation = animName;
+};
+
+const typeLetter = (letter) => {
+  if (tileNumber <= 4) {
+		let currentTile = guessTiles[tileNumber];
+    addAnimation(currentTile, 'pop');
+    currentTile.textContent = letter;
+    currentGuess.dataset.letters += letter;
+    tileNumber += 1;
+  };
+};
+
+const eraseLetter = () => {
+	if (tileNumber > 0) {
+		tileNumber -= 1;
+		let currentTile = guessTiles[tileNumber];
+		currentTile.textContent = '';
+		currentTile.dataset.animation = 'idle';
+		currentGuess.dataset.letters = currentGuess.dataset.letters.slice(0, -1);
+	};
+};
+
+const checkLetters = () => {
+	for (let i = 0; i < guessTiles.length; i++) {
+		if (guessTiles[i].textContent === words[0][i]) {
+			guessTiles[i].dataset.state = 'correct';
+		} else if (words[0].includes(guessTiles[i].textContent)) {
+			guessTiles[i].dataset.state = 'present';
+		} else {
+			guessTiles[i].dataset.state = 'absent';
+		};
+		addAnimation(guessTiles[i].parentElement, 'flip');
+	};
+};
+
+const checkGuess = () => {
+	if (currentGuess.dataset.guess === words[0] || currentGuessCount === 6) {
+		// remove event listener from document
+		controller.abort();
+		localStorage.clear();
+	} else {
+		tileNumber = 0;
+		currentGuessCount += 1;
+		currentGuess = document.querySelector(`#guess${currentGuessCount}`);
+		guessTiles = document.querySelectorAll(`#guess${currentGuessCount} .guess__tile`);
+	};
 }
 
+const addGuessToData = () => {
+	currentGuess.dataset.guess = currentGuess.dataset.letters;
+}
 
-document.addEventListener('keydown', (e) => {
-	// checking if the key is a letter
-	if (e.key.length === 1 && letterPattern.test(e.key)) {
-		if (currentTileNumber <= 4) {
-			// typing a letter in the current tile
-			guessTiles[currentTileNumber].innerHTML = e.key.toLocaleLowerCase();
-			// adding pop animation to the tile
-			guessTiles[currentTileNumber].dataset.animation = 'pop';
-			// adding a letter in the data-letters
-			currentGuess.dataset.letters += e.key.toLocaleLowerCase();
-			// increase tile counter
-			currentTileNumber += 1;
+const addGuessToStorage = () => {
+	localStorage.setItem('guess', [localStorage.getItem('guess'), currentGuess.dataset.guess]);
+}
+
+const submitGuess = () => {
+	checkLetters();
+	addGuessToData();
+	addGuessToStorage();
+	checkGuess();
+};
+
+window.addEventListener("load", () => {
+	addAnimDelay();
+	if (localStorage.getItem("guess")?.length > 0) {
+		for (let i = 1; i <= localStorage.getItem("guess").split(',').join('').length; i++){
+			typeLetter(localStorage.getItem("guess").split(',').join('')[i-1]);
+			if (i !== 0 && i % 5 === 0) {
+				checkLetters();
+				currentGuess.dataset.guess = currentGuess.dataset.letters;
+				checkGuess();
+			}
 		}
-		// erase the letter after pressing Backspace
-	} else if (e.key === 'Backspace') {
-		if (currentTileNumber > 0) {
-			// decrease tile counter
-			currentTileNumber -= 1;
-			// remove letter from current tile
-			guessTiles[currentTileNumber].innerHTML = '';
-			// remove pop animation from the tile
-			guessTiles[currentTileNumber].dataset.animation = 'idle';
-			// remove letter from data-letters
-			currentGuess.dataset.letters = currentGuess.dataset.letters.slice(0, -1);
-		}
-		// make a guess after pressing Enter
-	} else if (e.key === 'Enter' && currentTileNumber === 5 && currentGuessCount < 6) {
-		// tile counter reset
-		currentTileNumber = 0;
-		// increase guess counter  
-		currentGuessCount += 1;
-		// iteration over guess tiles
-		for (let i = 0; i < guessTiles.length; i++) {
-			// changing the color of the guess tiles
-			guessTiles[i].style.background = 'var(--tile)';
-			// adding flip animation to the tiles
-			guessTiles[i].parentElement.dataset.animation = 'flip';
-		}
-		// adding to the data-guess letters from data-letters
-		currentGuess.dataset.guess = currentGuess.dataset.letters;
-		// go to the next guess row
-		currentGuess = document.querySelector(`#guess${currentGuessCount}`);
-		// go to the next guess tiles 
-		guessTiles = document.querySelectorAll(`#guess${currentGuessCount} .guess__tile`);
 	}
 });
+
+document.addEventListener('keydown', (e) => {
+	if (e.key.length === 1 && letterPattern.test(e.key)) {
+		typeLetter(e.key.toLocaleLowerCase());
+	} else if (e.key === 'Backspace') {
+		eraseLetter();
+	} else if (e.key === 'Enter' && tileNumber === 5 && currentGuessCount <= 6) {
+		submitGuess();
+	};
+}, { signal });
